a76db46c30e873910ec5d02eb766d2dacb537e4e
Violet <vi@violet.wtf>
2020-07-14 14:09:31 -0700

    Move suggestPaper to info instead of warnings

--- src/main/java/io/papermc/lib/PaperLib.java
+++ src/main/java/io/papermc/lib/PaperLib.java
@@ -270,24 +270,24 @@ public class PaperLib {
         final String benefitsProperty = "paperlib.shown-benefits";
         final String pluginName = plugin.getDescription().getName();
         final Logger logger = plugin.getLogger();
-        logger.warning("====================================================");
-        logger.warning(" " + pluginName + " works better if you use Paper ");
-        logger.warning(" as your server software. ");
+        logger.info("====================================================");
+        logger.info(" " + pluginName + " works better if you use Paper ");
+        logger.info(" as your server software. ");
         if (System.getProperty(benefitsProperty) == null) {
             System.setProperty(benefitsProperty, "1");
-            logger.warning("  ");
-            logger.warning(" Paper offers significant performance improvements,");
-            logger.warning(" bug fixes, security enhancements and optional");
-            logger.warning(" features for server owners to enhance their server.");
-            logger.warning("  ");
-            logger.warning(" Paper includes Timings v2, which is significantly");
-            logger.warning(" better at diagnosing lag problems over v1.");
-            logger.warning("  ");
-            logger.warning(" All of your plugins should still work, and the");
-            logger.warning(" Paper community will gladly help you fix any issues.");
-            logger.warning("  ");
-            logger.warning(" Join the Paper Community @ https://papermc.io");
+            logger.info("  ");
+            logger.info(" Paper offers significant performance improvements,");
+            logger.info(" bug fixes, security enhancements and optional");
+            logger.info(" features for server owners to enhance their server.");
+            logger.info("  ");
+            logger.info(" Paper includes Timings v2, which is significantly");
+            logger.info(" better at diagnosing lag problems over v1.");
+            logger.info("  ");
+            logger.info(" All of your plugins should still work, and the");
+            logger.info(" Paper community will gladly help you fix any issues.");
+            logger.info("  ");
+            logger.info(" Join the Paper Community @ https://papermc.io");
         }
-        logger.warning("====================================================");
+        logger.info("====================================================");
     }
 }
